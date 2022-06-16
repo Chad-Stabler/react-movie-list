f09de6cff66e83c3f7d1629920ad7d5ea4b51659
Chad Stabler <stablerpsn@gmail.com>
2022-06-15 19:11:15 -0500

    set initial state values

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [visibleMovies, setVisibleMovies] = useState(allMovies);
+  const [movieFormYearReleased, setMovieFormYearReleased] = useState('');
+  const [movieFormTitle, setMovieFormTitle] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          Learn React
-        </a>
+        Welcome to your movie list
       </header>
+      <MovieForm />
     </div>
   );
 }
